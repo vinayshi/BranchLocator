e295d814a2b33e5c302e449ffa3017982e7a8fad
Vinay Shivanna <vinay.shivanna@photoninfotech.net>
2017-10-29 20:33:03 +0530

    first commit

---
+++ Branch Locator/ServicesHandler.swift
@@ -0,0 +1,9 @@
+//
+//  ServicesHandler.swift
+//  Branch Locator
+//
+//  Created by Vinay Shivanna on 10/28/17.
+//  Copyright © 2017 Vinay Shivanna. All rights reserved.
+//
+
+import Foundation
--- Branch Locator/StoreLocaterViewController.swift
+++ Branch Locator/StoreLocaterViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StoreLocaterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
